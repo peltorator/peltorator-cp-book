12c09e24b1729ce9d0804d1b57ff21f4199a3fd2
peltorator <peltorator@yandex.ru>
2021-10-29 16:52:37 +0300

    fix latex bugs

--- LaTeX/environment/stress/codes/good_stress.sh
+++ LaTeX/environment/stress/codes/good_stress.sh
@@ -6,8 +6,8 @@ g++ -std=c++17 -O2 gen.cpp -o gen
 for t in $(seq 1 100000); do
     echo "Running test $t"
     ./gen $t > input || { echo "gen failed"; exit; }
-    ./smart < input > smart_output || { echo "smart failed"; exit }
-    ./stupid < input > stupid_output || { echo "stupid failed"; exit }
+    ./smart < input > smart_output || { echo "smart failed"; exit; }
+    ./stupid < input > stupid_output || { echo "stupid failed"; exit; }
     diff smart_output stupid_output || { echo "outputs aren't equal"; exit; }
 done
 \end{code}
